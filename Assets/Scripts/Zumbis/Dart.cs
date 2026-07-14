d82be137bc12c39be7e7b729b42abea32392a0e7
flyingdragon <marco14963@gmail.com>
2026-07-14 15:45:47 -0300

    Dardo é atirado na horizontal

--- Assets/Scripts/Zumbis/Dart.cs
+++ Assets/Scripts/Zumbis/Dart.cs
@@ -28,12 +28,19 @@ public class Dart : MonoBehaviour {
         SetTarget(direction);
     }
 
+    private void HitEnemy(Enemy enemy) {
+
+    }
+
     public void SetTarget(Vector2 direction) {
         GameObject targetObj = new GameObject("Dart Target");
         target = targetObj.transform;
         targetObj.transform.parent = transform;
 
-        target.position = direction * 1000;
+        bool facingLeft = transform.position.x < 0;
+
+        target.position = new(direction.x + 100 * (facingLeft ? -1 : 1), direction.y, 0);
+        GetComponent<SpriteRenderer>().flipX = !facingLeft;
     }
 
     public void UpgradeStatus() => damage += 20f;
